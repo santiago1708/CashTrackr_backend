1c1b75cc504eed1e898c21e5582a53d0fbcf9144
santiago1708 <vela123tique@gmail.com>
2026-01-03 18:10:49 -0500

    conexion a la base de datos con orm Sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try { 
+        await db.authenticate() //Autenticar con la base de datos
+        db.sync() //Crear tablas y columnas en automatico
+
+        console.log( colors.blue.bold('Conexion exitosa a la BD'))
+    } catch (error) {
+        //console.log(error)
+        console.log( colors.red.bold('Conexion fallida a la BD'))
+    }
+}
+
+connectDB()
 
 const app = express()
 
