fbe036ec63611e363541c9df78ce66f3365ddf32
santiago1708 <vela123tique@gmail.com>
2026-01-05 21:52:54 -0500

    se agrego middleware y create

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -1,20 +1,28 @@
 import type { Request, Response } from 'express'
+import Budget from '../models/Budget'
 
 export class BudgetController {
 
-    static getAll = async (req : Request, res : Response) => {
+    static getAll = async (req: Request, res: Response) => {
         console.log('Desde /api/budgets')
     }
-    static create = async (req : Request, res : Response) => {
-        console.log('Desde Post /api/budgets')
+    static create = async (req: Request, res: Response) => {
+        try {
+            const budget = new Budget(req.body)
+            await budget.save()
+            res.status(201).json('Presupuesto creado correctamente')
+        } catch (error) {
+            //console.log(error)
+            res.status(500).json({ error: 'Ocurrio un error' })
+        }
     }
-    static getById = async (req : Request, res : Response) => {
+    static getById = async (req: Request, res: Response) => {
         console.log('Desde Get BY Id /api/budgets/:id')
     }
-    static updateById = async (req : Request, res : Response) => {
+    static updateById = async (req: Request, res: Response) => {
         console.log('Desde UpdateById /api/budgets/:id')
     }
-    static deleteById = async (req : Request, res : Response) => {
+    static deleteById = async (req: Request, res: Response) => {
         console.log('Desde DeleteById /api/budgets/:id')
     }
 
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express'
-import { BudgetController } from '../controllers/budgetController'
+import { body } from 'express-validator'
+import { BudgetController } from '../controllers/BudgetController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
 router.get('/', BudgetController.getAll)
-router.post('/', BudgetController.create)
+
+router.post('/', 
+    body('name')
+        .notEmpty().withMessage('El campo nombre no puede ir vacio'),
+    body('amount')
+        .notEmpty().withMessage('La cantidad del presupuesto no puede ir vacia')
+        .isNumeric().withMessage('Cantidad no valida')
+        .custom(value => value > 0).withMessage('El presupuesto debe ser mayor a 0'),
+    handleInputErrors,
+    BudgetController.create)
 router.get('/:id', BudgetController.getById)
 router.put('/:id', BudgetController.updateById)
 router.delete('/:id', BudgetController.deleteById)
