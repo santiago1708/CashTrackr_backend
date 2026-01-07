745ad788add1ffa8e38542736a562538a98c2ed3
santiago1708 <vela123tique@gmail.com>
2026-01-07 16:49:34 -0500

    creacion de middleware para input del budget y optimizacion

--- src/middleware/budget.ts
+++ src/middleware/budget.ts
@@ -1,5 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
-import { param, validationResult } from "express-validator";
+import { body, param, validationResult } from "express-validator";
 import Budget from "../models/Budget";
 
 declare global {
@@ -10,33 +10,53 @@ declare global {
     }
 }
 
-export const validateBudgetId =async (req: Request, res: Response, next: NextFunction) => {
-
-    await param('id')
+export const validateBudgetId = async (req: Request, res: Response, next: NextFunction) => {
+    await param('budgetId')
         .isInt().withMessage('Id No valido')
         .custom(value => value > 0).withMessage('id no valido')
         .run(req)
-
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
     next()
 }
 
-export const validateBudgetExists =async (req: Request, res: Response, next: NextFunction) => {
+export const validateBudgetExists = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-            const { id } = req.params
-            const budget = await Budget.findByPk(id, {})
+        const { budgetId } = req.params
+        const budget = await Budget.findByPk(budgetId, {})
 
-            if (!budget) {
-                const error = new Error('Presupuesto no encontrado')
-                res.status(404).json({ error: error.message })
-                return
-            }
-
-            req.budget = budget
-            next()
-        } catch (error) {
-            //console.log(error)
-            res.status(500).json({ error: 'Ocurrio un error' })
+        if (!budget) {
+            const error = new Error('Presupuesto no encontrado')
+            res.status(404).json({ error: error.message })
+            return
         }
 
+        req.budget = budget
+        next()
+    } catch (error) {
+        //console.log(error)
+        res.status(500).json({ error: 'Ocurrio un error' })
+    }
+
+}
+
+export const validateBudgetInput = async (req: Request, res: Response, next: NextFunction) => {
+
+    await body('name')
+            .notEmpty().withMessage('El campo nombre no puede ir vacio')
+            .run(req),
+    await body('amount')
+            .notEmpty().withMessage('La cantidad del presupuesto no puede ir vacia')
+            .isNumeric().withMessage('Cantidad no valida')
+            .custom(value => value > 0).withMessage('El presupuesto debe ser mayor a 0')
+            .run(req)
+
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    next()
 }
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -2,44 +2,28 @@ import { Router } from 'express'
 import { body, param } from 'express-validator'
 import { BudgetController } from '../controllers/BudgetController'
 import { handleInputErrors } from '../middleware/validation'
-import { validateBudgetExists, validateBudgetId } from '../middleware/budget'
+import { validateBudgetExists, validateBudgetId, validateBudgetInput } from '../middleware/budget'
 
 const router = Router()
+
+router.param('budgetId', validateBudgetId)
+router.param('budgetId', validateBudgetExists)
 
 router.get('/', BudgetController.getAll)
 
 router.post('/',
-    body('name')
-        .notEmpty().withMessage('El campo nombre no puede ir vacio'),
-    body('amount')
-        .notEmpty().withMessage('La cantidad del presupuesto no puede ir vacia')
-        .isNumeric().withMessage('Cantidad no valida')
-        .custom(value => value > 0).withMessage('El presupuesto debe ser mayor a 0'),
+    
     handleInputErrors,
     BudgetController.create)
 
-router.get('/:id',
-    validateBudgetId,
-    validateBudgetExists,
-    handleInputErrors,
+router.get('/:budgetId',
     BudgetController.getById)
 
-router.put('/:id',
-    validateBudgetId,
-    validateBudgetExists,
-    body('name')
-        .notEmpty().withMessage('El campo nombre no puede ir vacio'),
-    body('amount')
-        .notEmpty().withMessage('La cantidad del presupuesto no puede ir vacia')
-        .isNumeric().withMessage('Cantidad no valida')
-        .custom(value => value > 0).withMessage('El presupuesto debe ser mayor a 0'),
-    handleInputErrors,
+router.put('/:budgetId',
+    validateBudgetInput,
     BudgetController.updateById)
 
-router.delete('/:id',
-    validateBudgetId,
-    handleInputErrors,
-    validateBudgetExists,
+router.delete('/:budgetId',
     BudgetController.deleteById)
 
 
