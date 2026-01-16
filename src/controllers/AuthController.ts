e7eb664788798aa465669d95fc4845a97ae10796
santiago1708 <vela123tique@gmail.com>
2026-01-15 19:37:08 -0500

    se creo un usuario con hash en contraseña y generando un token para confimacion

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,29 @@
 import type { Request, Response } from 'express'
+import User from '../models/User'
+import { hashPassword } from '../utils/Auth'
+import { generateToken } from '../utils/token'
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
-        
+        const {email, password} = req.body
+        try {
+            const userEmailExist = await User.findOne({ where: { email } }) // buscar si el email ya existe
+            
+            if (userEmailExist) { // si existe, retornar error
+                const error = new Error('El correo ya esta registrado')
+                res.status(409).json({ error: error.message })
+                return 
+            }
+            
+            const user = new User(req.body) 
+            user.password = await hashPassword(password)
+            user.token = generateToken()
+            await user.save()
+            res.json('Usuario creado correctamente!')
+        } catch (e) {
+            const error = new Error('Hubo un error')
+            res.status(500).json({ error: error.message })
+            return
+        }
     }
 }
--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { AuthController } from '../controllers/AuthController'
+import { handleInputErrors } from '../middleware/validation'
 
 const routerAuth = Router()
 
-routerAuth.post('/create-account', AuthController.createAccount)
+routerAuth.post('/create-account',
+    body('name')
+        .notEmpty().withMessage('El nombre es obligatorio'),
+    body('email')
+        .notEmpty().withMessage('El correo electronico es obligatorio')
+        .isEmail().withMessage('El email no es valido'),
+    body('password')
+        .notEmpty().withMessage('La contraseña es obligatoria')
+        .isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
+    handleInputErrors,
+    AuthController.createAccount)
 
 
 
