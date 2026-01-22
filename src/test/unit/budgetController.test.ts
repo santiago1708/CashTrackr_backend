8e872776f23088602451172830c63beece3e3edf
santiago1708 <vela123tique@gmail.com>
2026-01-22 17:13:25 -0500

    se optimizo aun mas los test

--- src/test/unit/budgetController.test.ts
+++ src/test/unit/budgetController.test.ts
@@ -7,18 +7,55 @@ jest.mock('../../models/Budget', () => ({
 }))
 
 describe('BudgetController.getAll', () => {
-    it('should retrive 2 budgets for user with id 1 ', async () => {
 
+    beforeEach(() => {
+        (Budget.findAll as jest.Mock).mockImplementation((options) => {
+            const updatedBudgets = budgets.filter(budget => budget.userId === options.where.UserId)
+            return Promise.resolve(updatedBudgets)
+        })
+    })
+
+    it('should retrive 2 budgets for user with id 1 ', async () => {
         const req = createRequest({
-            method: 'GET',
-            utl: '/api/budgets',
+            method: 'GET', //No es importante pero es mas explicito
+            utl: '/api/budgets', // NO es importante pero es mas explicito
             user: { id: 1 }
         })
         const res = createResponse(); //Importante este ;
-
-        (Budget.findAll as jest.Mock).mockResolvedValue(budgets.filter(budget => budget.userId === req.user.id))
+        
         await BudgetController.getAll(req, res)
         const data = res._getJSONData()
         expect(data).toHaveLength(2)
+        expect(res.statusCode).toBe(200)
+        expect(res.status).not.toBe(404)
     })
+    it('should retrive 1 budgets for user with id 2 ', async () => {
+        const req = createRequest({
+            method: 'GET', //No es importante pero es mas explicito
+            utl: '/api/budgets', // NO es importante pero es mas explicito
+            user: { id: 2 }
+        })
+        const res = createResponse(); //Importante este ;
+
+        await BudgetController.getAll(req, res)
+        const data = res._getJSONData()
+        expect(data).toHaveLength(1)
+        expect(res.statusCode).toBe(200)
+        expect(res.status).not.toBe(404)
+    })
+    it('should retrive 0 budgets for user with id 10 ', async () => {
+        const req = createRequest({
+            method: 'GET', //No es importante pero es mas explicito
+            utl: '/api/budgets', // NO es importante pero es mas explicito
+            user: { id: 10 }
+        })
+        const res = createResponse(); //Importante este ;
+
+        await BudgetController.getAll(req, res)
+        const data = res._getJSONData()
+        expect(data).toHaveLength(0)
+        expect(res.statusCode).toBe(200)
+        expect(res.status).not.toBe(404)
+    })
+
 })
