ea662fa90fbdcdc71d922c2884614716bd1ca3d6
santiago1708 <vela123tique@gmail.com>
2026-01-30 20:09:45 -0500

    Completado el test del middleware 'validateBudgetExists'

--- src/test/unit/middleware/budget.test.ts
+++ src/test/unit/middleware/budget.test.ts
@@ -1,6 +1,7 @@
 import { createRequest, createResponse } from 'node-mocks-http'
 import { validateBudgetExists } from '../../../middleware/budget'
 import Budget from '../../../models/Budget'
+import { budgets } from '../../mocks/budgets'
 
 jest.mock('../../../models/Budget', () => ({
     findByPk: jest.fn()
@@ -8,7 +9,7 @@ jest.mock('../../../models/Budget', () => ({
 
 describe('Budget - validateBudgetExists', () => {
     it('Should handle non-existent budget', async () => {
-        (Budget.findByPk as jest.Mock).mockResolvedValue(null)
+        (Budget.findByPk as jest.Mock).mockResolvedValue(null) //Forza un error valido
         const req = createRequest({
             params: {
                 budgetId: 1
@@ -20,7 +21,39 @@ describe('Budget - validateBudgetExists', () => {
         const data = res._getJSONData()
         //Assert 
         expect(res.statusCode).toBe(404)
-        expect(data).toStrictEqual({error: 'Presupuesto no encontrado'})
+        expect(data).toStrictEqual({ error: 'Presupuesto no encontrado' })
         expect(next).not.toHaveBeenCalled() //Importante que el middlaware no mande a llamar el next()
+    })
+
+    it('Should proced to next middleware if budget exists', async () => {
+        (Budget.findByPk as jest.Mock).mockResolvedValue(budgets[0])
+        const req = createRequest({
+            params: {
+                budgetId: 1
+            }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+
+        await validateBudgetExists(req, res, next)
+        expect(next).toHaveBeenCalled() //Se mando a llamar el next
+        expect(req.budget).toEqual(budgets[0])
+    })
+
+    it('Should handle non-existent budget', async () => {
+        (Budget.findByPk as jest.Mock).mockRejectedValue(new Error) //Forza el error hacia el catch
+        const req = createRequest({
+            params: {
+                budgetId: 1
+            }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+        await validateBudgetExists(req, res, next)
+        const data = res._getJSONData()
+        //Assert 
+        expect(res.statusCode).toBe(500)
+        expect(data).toEqual({ error: 'Ocurrio un error' })
+        expect(next).not.toHaveBeenCalled()
     })
 })
