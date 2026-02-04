8fbf97c82f3042c10a9c19f110e8cc37c2827ad5
santiago1708 <vela123tique@gmail.com>
2026-02-03 19:26:40 -0500

    Testing: middleware validateExpenseExist - verify acces user if wanna create a new expense

--- src/test/unit/middleware/expense.test.ts
+++ src/test/unit/middleware/expense.test.ts
@@ -2,6 +2,8 @@ import { createRequest, createResponse } from 'node-mocks-http'
 import Expense from '../../../models/Expense'
 import { expenses } from '../../mocks/expenses'
 import { validateExpenseExits } from '../../../middleware/expense'
+import { hasAcces } from '../../../middleware/budget'
+import { budgets } from '../../mocks/budgets'
 
 jest.mock('../../../models/Expense', () => ({
     findByPk: jest.fn()
@@ -9,9 +11,9 @@ jest.mock('../../../models/Expense', () => ({
 
 describe('Expense - ValidateExpenseExits', () => {
 
-    beforeEach( () => {
+    beforeEach(() => {
         (Expense.findByPk as jest.Mock).mockImplementation((expenseId) => {
-            const expense = expenses.filter( e => e.id === expenseId)[0] ?? null
+            const expense = expenses.filter(e => e.id === expenseId)[0] ?? null
             return Promise.resolve(expense)
         })
     })
@@ -40,7 +42,7 @@ describe('Expense - ValidateExpenseExits', () => {
         const data = res._getJSONData()
 
         expect(res.statusCode).toBe(404)
-        expect(data).toEqual({error: 'Gasto no encontrado'})
+        expect(data).toEqual({ error: 'Gasto no encontrado' })
         expect(next).not.toHaveBeenCalled()
     })
 
@@ -55,7 +57,27 @@ describe('Expense - ValidateExpenseExits', () => {
         const data = res._getJSONData()
 
         expect(res.statusCode).toBe(500)
-        expect(data).toEqual({error: 'Hubo un error'})
+        expect(data).toEqual({ error: 'Hubo un error' })
+        expect(next).not.toHaveBeenCalled()
+    })
+
+
+    // Testing de integracion
+    it('Should prevent unauthorized users from addign expenses', async () => {
+        const req = createRequest({
+            method: 'POST',
+            url: '/api/budgets/:budgetId/expenses',
+            budget: budgets[0],
+            user: { id: 20 },
+            body: { name: 'Expense test', amount: 200 }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+        hasAcces(req, res, next)
+
+        const data = res._getJSONData()
+        expect(res.statusCode).toBe(401)
+        expect(data).toEqual({error: 'No tienes acceso a este presupuesto'})
         expect(next).not.toHaveBeenCalled()
     })
 })
