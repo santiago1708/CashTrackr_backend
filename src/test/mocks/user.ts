78f6ee9cd25518b7fea8bd19a10b0057c6ec2842
santiago1708 <vela123tique@gmail.com>
2026-02-03 21:10:42 -0500

    create a mocks of users

---
+++ src/test/mocks/user.ts
@@ -0,0 +1,22 @@
+export const users = [
+    {
+        "id": 1,
+        "name": "David Tique",
+        "password": "$2b$10$NAgr5hA3yDElXn.mNhZvX.qChniFsL1gA/pRJjYYVK/985U1Wlnfe",
+        "email": "svela@gmail.com",
+        "token": "",
+        "confirmed": true,
+        "createdAt": "2026-02-04 02:01:18.301+00",
+        "updatedAt": "2026-02-04 02:02:43.084+00"
+    },
+    {
+        "id": 2,
+        "name": "Santiago Vela",
+        "password": "$2b$10$YMOhT/rauZFoMSXLoraOjupBXp6dupdCPdBt3BtW0LhCDfHIPf.s2",
+        "email": "vela@gmail.com",
+        "token": "",
+        "confirmed": true,
+        "createdAt": "2026-02-04 02:06:31.912+00",
+        "updatedAt": "2026-02-04 02:06:46.55+00"
+    }
+]
